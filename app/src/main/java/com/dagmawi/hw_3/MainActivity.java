3aa91fcb46648dd6057d8dd1ce4ff78fe054c989
Dagmawi Demissie <dagim.dawit17@gmail.com>
2021-05-28 14:35:40 -0700

    HW-3Complete. Adding missing folder

---
+++ app/src/main/java/com/dagmawi/hw_3/MainActivity.java
@@ -0,0 +1,42 @@
+package com.dagmawi.hw_3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendConfirmation(View view) {
+        Toast toast = Toast.makeText(this, R.string.send_confirmation, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+
+    public void runMovies(View view) {
+        Intent intent = new Intent(this, Movies.class);
+        startActivity(intent);
+    }
+
+    public void showMessageTwo(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_two, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void showMessageThree(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_three, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void showMessageFour(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_four, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
